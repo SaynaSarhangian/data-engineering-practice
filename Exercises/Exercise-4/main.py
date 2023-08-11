7f34ca90540a9fe64d27b3807c87ca6a5a59c2ff
SaynaSarhangian <sayna.sarhangian1@gmail.com>
2023-08-11 17:01:34 +1000

    Solution-4-added some comments

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -4,16 +4,7 @@ import os
 import pandas as pd
 
 
-# def find_json():
-#     hits = glob("**/*.json", recursive=True)
-#     json_list = []
-#     for item in hits:
-#         json_list.append(item.split('\\')[-1])
-#     print(f'the json files are: {json_list}')
-#     # ['file-1.json', 'file-4.json', 'file-3.json', 'file-2.json']
-#     return json_list
-
-# passing flatten_json a loaded json file and it returns a new dictionary of that file content
+# passing flatten_json a loaded json file and it returns an amended dictionary of that file contents
 def flatten_json(json_obj, parent_key='', separator='_'):
     items = {}
     for key, value in json_obj.items():
@@ -37,16 +28,29 @@ if __name__ == "__main__":
         for j in j_files:
             if j.endswith('.json'):
                 file_path = os.path.join(root, j)  # join one or more path segments to create a complete path.
-                print("Reading file as a result of os.path.join():", file_path)
+                print("Reading json file and ready to open:", file_path)
                 with open(file_path, 'r') as json_file:
                     data = json.load(json_file)
+                    print(f'f loading json file: {json_file}')
                     flattened = flatten_json(data)  # pass the loaded json file to flattening function
-                    flattened_data.append(flattened)  # append each flattened file to a list
+                    flattened_data.append(flattened)  # append each flattened file (dict) to a list
     print(f'here is the resulting list of flattened json files with length of {len(flattened_data)}: {flattened_data}')
     df = pd.DataFrame(flattened_data)
     print(f'the columns/headers of the resulting df are:{df.columns}')
     output_csv_file = 'output_csv_file.csv'
     df.to_csv(output_csv_file, index=False)
-# If recursive is true, the pattern “**” will match any files and zero or more directories, subdirectories and symbolic links to directories.
+
 # Flattening a JSON file refers to the process of converting a nested or hierarchical JSON structure
 # into a simpler, tabular format where each key-value pair is represented as a separate column in a flat structure. insted of having a nested value.
+#could have used pd.json_normalize() to flatten instead of the func.
+
+
+#make a list of all json files in current directory
+# def find_json():
+#     hits = glob("**/*.json", recursive=True)## If recursive is true, the pattern “**” will match any files and zero or more directories, subdirectories and symbolic links to directories.
+#     json_list = []
+#     for item in hits:
+#         json_list.append(item.split('\\')[-1])
+#     print(f'the json files are: {json_list}')
+#     # ['file-1.json', 'file-4.json', 'file-3.json', 'file-2.json']
+#     return json_list
