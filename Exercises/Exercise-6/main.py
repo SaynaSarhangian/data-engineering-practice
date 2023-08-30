18142b56828075958d0f80562f5521e55e0b2bc7
SaynaSarhangian <sayna.sarhangian1@gmail.com>
2023-08-30 23:21:04 +1000

    Solution-6-part-1(edited reading byte file with sc.parallelize)

--- Exercises/Exercise-6/main.py
+++ Exercises/Exercise-6/main.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 import os
 import sys
-from spark_util import read_zip, transform1
+from spark_util import read_zip, transform1, transform2, zip_extract
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
@@ -14,27 +14,30 @@ def main():
     output_path = "reports"
 
     df1 = read_zip('data/Divvy_Trips_2019_Q4.zip', spark)
-    #df2 = read_zip(path, z_list[1], spark)
+    # df2 = read_zip('data/Divvy_Trips_2020_Q1.zip', spark)
 
-    df_avg_tripduration = transform1(df1, spark)
-    print('******Showing a preview of the transformed dataframe:...')
+    df1, df_avg_tripduration = transform1(df1, spark)
     df_avg_tripduration.show(25)
+
+    # export df_avg_tripduration to csv:
+    # df_avg_tripduration.coalesce(1).write \
+    #     .option("header", True) \
+    #     .option("mode", "overwrite") \
+    #     .csv(output_path)
+
+    df1.printSchema()
     print('Count of rows:', df_avg_tripduration.count())
+    # need the transformed df1 to do the second question
 
-    df_avg_tripduration.coalesce(1).write \
-        .option("header", True) \
-        .option("mode", "overwrite") \
-        .csv(output_path)
-
-    #print(df1.trip_id.nunique())
-    #print(df1.distinct().count())
-    # df_avg_tripduration.printSchema()
-
+    df_trip_count_per_day = transform2(df1, spark)
+    df_trip_count_per_day.show(7)
 
 
 if __name__ == "__main__":
     main()
-    #Spark creates a folder with the name specified in the path and writes data as multiple parts(partitions)
+    # there's no straightforward way to read .zip files directly in pyspark.
+    # df.write.csv(path) in Spark creates a folder with the name specified in the path and writes data as multiple parts(partitions).
+    # we only need a single csv as export, so used df.coalesce(1).write()
     # 1.to run Apache Spark locally, winutils.exe is required for Windows: installed pyspark and winutils.exe
     # 2.set env variables
     # 3.read one .csv file from each .zip file and load onto df
--- Exercises/Exercise-6/spark_util.py
+++ Exercises/Exercise-6/spark_util.py
@@ -1,6 +1,7 @@
 from zipfile import ZipFile
 import os
-
+import io
+import zipfile
 from pyspark.sql.functions import to_date, col
 
 
@@ -9,14 +10,18 @@ def read_zip(zip_path, spark):
         for csv_file in zip.namelist():
             # only consider the required csv file
             if csv_file.startswith('Divvy'):
-                csv_data = zip.read(csv_file)
+                csv_data = zip.read(csv_file)  # this give the content of the file in byte array
+                csv_data_str = str(csv_data, 'utf-8')  # this converts the file content from byte array to string
+                file_lines = csv_data_str.splitlines()  # this turns the file content to a list of lines (list of strings)
                 # Convert CSV data into a DataFrame
-                csv_to_df = spark.sparkContext.parallelize(csv_data.splitlines())
+                csv_to_df = spark.sparkContext.parallelize(file_lines) # this reads the lines into a RDD
                 df = spark.read.csv(csv_to_df, header=True, inferSchema=True)
                 print(f'reading csv file {csv_file} into a dataframe')
                 return df
 
 
+
+# add a converted date col to df1, group by and give avg trip duration per day
 def transform1(df1, spark):
     # add a new converted_start_time with date type from an existing col
     df1 = df1.withColumn("converted_start_time", to_date(col("start_time"), "yyyy-MM-dd HH:mm:ss"))
@@ -32,9 +37,10 @@ order by \
     converted_start_time 
 '''
     df_avg_tripduration = spark.sql(query)
-    return df_avg_tripduration
+    return df1, df_avg_tripduration
+
 
 def transform2(df1, spark):
-
-
+    df_trip_count_per_day = df1.groupby('converted_start_time', b'trip_id').count()
+    return df_trip_count_per_day
 
