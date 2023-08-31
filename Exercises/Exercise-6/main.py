b8c8a3aa900f8d988fcd3e2eaf741bcb7c81a7b6
SaynaSarhangian <sayna.sarhangian1@gmail.com>
2023-08-31 13:17:25 +1000

    Solution-6-part-2 added-part the station with max trips in general

--- Exercises/Exercise-6/main.py
+++ Exercises/Exercise-6/main.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 import os
 import sys
-from spark_util import read_zip, transform1, transform2, zip_extract
+from spark_util import read_zip, transform1, transform3
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
@@ -11,26 +11,15 @@ def main():
     spark = SparkSession.builder.appName("Exercise6").enableHiveSupport().getOrCreate()
 
     path = 'data'
+    # .csv export location
     output_path = "reports"
-
     df1 = read_zip('data/Divvy_Trips_2019_Q4.zip', spark)
     # df2 = read_zip('data/Divvy_Trips_2020_Q1.zip', spark)
 
-    df1, df_avg_tripduration = transform1(df1, spark)
-    df_avg_tripduration.show(25)
-
-    # export df_avg_tripduration to csv:
-    # df_avg_tripduration.coalesce(1).write \
-    #     .option("header", True) \
-    #     .option("mode", "overwrite") \
-    #     .csv(output_path)
-
-    df1.printSchema()
-    print('Count of rows:', df_avg_tripduration.count())
-    # need the transformed df1 to do the second question
-
-    df_trip_count_per_day = transform2(df1, spark)
-    df_trip_count_per_day.show(7)
+    df1, df_avg_tripduration = transform1(df1, spark, output_path)
+    # df_trip_count_per_day = transform2(df1, spark, output_path)
+    df_pop_trip_station = transform3(df1, spark, output_path)
+    df_pop_trip_station.show(30)
 
 
 if __name__ == "__main__":
@@ -45,7 +34,9 @@ if __name__ == "__main__":
     # Q1.convert  start_time column to date type, group by date, calculate avg duration per day
 
     # print(df1.columns)
-    # ["b'trip_id", 'start_time', 'end_time', 'bikeid', 'tripduration', 'from_station_id', 'from_station_name', 'to_station_id', 'to_station_name', 'usertype', 'gender', "birthyear'"]
+    # ['trip_id', 'start_time', 'end_time', 'bikeid', 'tripduration', 'from_station_id', 'from_station_name', 'to_station_id', 'to_station_name', 'usertype', 'gender', 'birthyear']
 
     # print(df2.columns)
-    # ["b'ride_id", 'rideable_type', 'started_at', 'ended_at', 'start_station_name', 'start_station_id', 'end_station_name', 'end_station_id', 'start_lat', 'start_lng', 'end_lat', 'end_lng', "member_casual'"]
+    # ['ride_id', 'rideable_type', 'started_at', 'ended_at', 'start_station_name', 'start_station_id', 'end_station_name', 'end_station_id', 'start_lat', 'start_lng', 'end_lat', 'end_lng', 'member_casual']
+
+    # df1 and df2 have start_station_name|start_station_id|end_station_name|end_station_id in common.
--- Exercises/Exercise-6/spark_util.py
+++ Exercises/Exercise-6/spark_util.py
@@ -1,8 +1,6 @@
 from zipfile import ZipFile
-import os
-import io
-import zipfile
-from pyspark.sql.functions import to_date, col
+from pyspark.sql.functions import *
+from pyspark.sql import functions as F
 
 
 def read_zip(zip_path, spark):
@@ -10,20 +8,22 @@ def read_zip(zip_path, spark):
         for csv_file in zip.namelist():
             # only consider the required csv file
             if csv_file.startswith('Divvy'):
-                csv_data = zip.read(csv_file)  # this give the content of the file in byte array
-                csv_data_str = str(csv_data, 'utf-8')  # this converts the file content from byte array to string
+                csv_data = zip.read(csv_file)
+                print(
+                    f'extract from .zip is in {type(csv_data)} format. converting to string....')  # this give the content of the file in byte array
+                csv_data_str = str(csv_data,
+                                   'utf-8')  # this converts the file content (byte string starting with b') to string using UTF-8 encoding
                 file_lines = csv_data_str.splitlines()  # this turns the file content to a list of lines (list of strings)
                 # Convert CSV data into a DataFrame
-                csv_to_df = spark.sparkContext.parallelize(file_lines) # this reads the lines into a RDD
+                csv_to_df = spark.sparkContext.parallelize(file_lines)  # this reads the lines into a RDD
+                print(f'extract type is now {type(csv_to_df)}. converting to dataframe and returning it....')
                 df = spark.read.csv(csv_to_df, header=True, inferSchema=True)
-                print(f'reading csv file {csv_file} into a dataframe')
                 return df
 
 
-
-# add a converted date col to df1, group by and give avg trip duration per day
-def transform1(df1, spark):
-    # add a new converted_start_time with date type from an existing col
+# add a converted date col to df1, give avg trip duration per day, return the result and transformed df1, export result to .csv
+def transform1(df1, spark, output_path):
+    # add a new col converted_start_time with date type from an existing col to df1
     df1 = df1.withColumn("converted_start_time", to_date(col("start_time"), "yyyy-MM-dd HH:mm:ss"))
     # sparkSQL to calculate Question1:What is the average trip duration per day?
     df1.createOrReplaceTempView("df1_table")
@@ -37,10 +37,29 @@ order by \
     converted_start_time 
 '''
     df_avg_tripduration = spark.sql(query)
+    # export df_avg_tripduration to csv:
+    # df_avg_tripduration.coalesce(1).write \
+    #     .option("header", True) \
+    #     .option("mode", "overwrite") \
+    #     .csv(output_path)
     return df1, df_avg_tripduration
 
 
-def transform2(df1, spark):
-    df_trip_count_per_day = df1.groupby('converted_start_time', b'trip_id').count()
+# How many trips (trip id) were taken per day?
+def transform2(df1, spark, output_path):
+    df_trip_count_per_day = df1.groupby('converted_start_time').agg({'trip_id': 'count'}).sort('converted_start_time')
+    # export to .csv:
+    # df_trip_count_per_day.coalesce(1).write \
+    #     .option("header", True) \
+    #     .option("mode", "overwrite") \
+    #     .csv(output_path)
     return df_trip_count_per_day
 
+
+# the most popular starting trip station for each month:
+def transform3(df1, spark, output_path):
+    df_pop_trip_station = df1.groupby('from_station_id', 'from_station_name').agg(
+        count('trip_id').alias('count_trip_id')).sort(col('count_trip_id').desc())
+    first_row = df_pop_trip_station.collect()[0]
+    print(f'***********************The most popular station is {first_row[0]} with name {first_row[1]} and the max trips for it is {first_row[2]}!!!!!!!!!!!!!!!!!!!!!!!!!')
+    return df_pop_trip_station
