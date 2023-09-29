a3ad87067fe27da9e4d96fbc9128a76ac89c14bd
SaynaSarhangian <sayna.sarhangian1@gmail.com>
2023-09-29 10:19:08 +1000

    Solution-6-finalized-1

--- Exercises/Exercise-6/spark_util.py
+++ Exercises/Exercise-6/spark_util.py
@@ -1,29 +1,37 @@
+import os, shutil
 from zipfile import ZipFile
 from pyspark.sql.functions import *
 from pyspark.sql.window import Window
 from pyspark.sql.functions import row_number
+# from pyspark.sql import Row
+from pyspark import SparkConf, SparkContext
 
 
-def read_zip(zip_path, spark):
+def create_dir_check_exists(created_dir):
+    base_dir = "D:/3-repos/data-engineering-practice/Exercises/Exercise-6/"
+    if os.path.exists(created_dir):
+        shutil.rmtree(created_dir)
+    os.makedirs(created_dir)
+    print(f'new directory {created_dir} was created!!')
+
+
+def read_zip(zip_path, sc, spark):
     with ZipFile(zip_path, 'r') as zip:
-        for csv_file in zip.namelist():
-            # only consider the required csv file
-            if csv_file.startswith('Divvy'):
-                csv_data = zip.read(csv_file)
-                print(
-                    f'extract from .zip is in {type(csv_data)} format. converting to string....')  # this give the content of the file in byte array
-                csv_data_str = str(csv_data,
-                                   'utf-8')  # this converts the file content (byte string starting with b') to string using UTF-8 encoding
-                file_lines = csv_data_str.splitlines()  # this turns the file content to a list of lines (list of strings)
-                # Convert CSV data into a DataFrame
-                csv_to_df = spark.sparkContext.parallelize(file_lines)  # this reads the lines into a RDD
-                print(f'extract type is now {type(csv_to_df)}. converting to dataframe and returning it....')
-                df = spark.read.csv(csv_to_df, header=True, inferSchema=True)
-                return df
+        csv_file = zip.namelist()[0]  # 'Divvy_Trips_2019_Q4.csv'
+        csv_file_bytes = zip.read(csv_file)
+        # print(f'type of the read csv file: {type(csv_file_bytes)}')
+        csv_file_str = str(csv_file_bytes,
+                           'utf-8')  # this converts the file content (byte string starting with b') to string using UTF-8 encoding
+        # print(f'type of converted csv file: {type(csv_file_str)}')
+        csv_file_lines = csv_file_str.splitlines()  # (list of lines split by \n  num of lines:704056 including the header)
+        rdd = sc.parallelize(csv_file_lines)  # passing a list to sc.parallelize()
+        # Convert rdd into a spark DataFrame
+        df1 = spark.read.csv(rdd, header=True, inferSchema=True)
+        return df1
 
 
-# add a converted date col to df1, give avg trip duration per day, return the result and transformed df1, export result to .csv
-def transform1(df1, spark, output_path):
+# add cols, calculate avg trip duration per day, return the result and transformed df1, export result to .csv
+def transform1(df1, spark, created_dir):
     # add a new col converted_start_time with date type from an existing col to df1
     df1 = df1.withColumn("converted_start_time", to_date(col("start_time"), "yyyy-MM-dd HH:mm:ss"))
     # add age column
@@ -46,10 +54,10 @@ order by \
     # 1.What is the average trip duration per day?
     df_avg_tripduration = spark.sql(query)
     # export df_avg_tripduration to csv:
-    # df_avg_tripduration.coalesce(1).write \
-    #     .option("header", True) \
-    #     .option("mode", "overwrite") \
-    #     .csv(output_path)
+    df_avg_tripduration.coalesce(1).write\
+        .mode('overwrite')\
+        .option('header', True) \
+        .csv(created_dir)
     return df1, df_avg_tripduration
 
 
@@ -83,7 +91,6 @@ def transform3(df1, spark, output_path):
     df_pop_trip_station = df_pop_trip_station.withColumn('row_number', row_number().over(window_agg)) \
         .withColumn('max', max(col('count_trip_id')).over(window_agg)) \
         .filter(col('row_number') == 1).drop('row_number')
-    df_pop_trip_station.show()
     # print(f'num of rows for this df are: {df_pop_trip_station.count()}')  # 1783
     return df_pop_trip_station
 
@@ -99,16 +106,16 @@ def transform4(df1, spark, output_path):
         .filter(col('row_number') <= 3).sort(col('converted_start_time').desc())
     return df_top3_stations
 
+
 def transform5(df1, spark, output_path):
     # 5.Do Males or Females take longer trips on average?
     df_gender_avgduration = df1.groupby('gender').agg(round(avg('tripduration'), 2).alias('avg_tripduration'))
-    print(f'Do Males or Females take longer trips on average?:')
-    df_gender_avgduration.show()
     return df_gender_avgduration
+
+
+# 6.What is the top 10 ages of those that take the longest trips, and shortest?
 def transform6(df1, spark, output_path):
-    # 6.What is the top 10 ages of those that take the longest trips, and shortest?
     window_top10_ages = Window.partitionBy('age').orderBy(col('tripduration').desc())
     df_top10_ages = df1.groupby('age').agg(max('tripduration').alias('max_tripduration')).orderBy(
         col('max_tripduration').desc())
-    df_top10_ages.show(40)
     return df_top10_ages
